981783ecc21d7d1d800c9d02c1322c8238921d6d
VidadiMurad <vidad0murad@gmail.com>
2026-04-18 13:57:47 +0400

    I update some codes in my project

--- App.java
+++ App.java
@@ -9,26 +9,31 @@ public class App {
         
         // Variables
         double amount;
-        double kdvRatio;
-        double kdvAmount;
-        double kdvResult;
+        double kdvAmount = 0;
+        double kdvResult = 0;
+        double kdvRatio = 18;
 
         // Labels for get data from user
         System.out.print("Enter Your Amount:");
         amount = scanner.nextDouble();
-        System.out.print("Enter the ratio of KDV:");
-        kdvRatio = scanner.nextDouble();
 
-       // Operations
-       kdvAmount = amount * (kdvRatio / 100);
-       kdvResult = amount + kdvAmount;
 
-       // Opeations fot INT -> DOUBLE
-       //double newKdvAmount = (int) kdvAmount;
-       //double newKdvResult = (int) kdvResult;
+
+        // Statesments and Oprations
+        if(amount > 0 && amount < 500){
+            kdvRatio = 18;
+            kdvAmount = amount * (kdvRatio / 100);
+            kdvResult = amount + kdvAmount;
+        }else if( amount > 500){
+            kdvRatio = 8;
+            kdvAmount = amount * (kdvRatio / 100);
+            kdvResult = amount + kdvAmount;
+        }
+
 
        // Result
- 
+        
+       
        System.out.println("---------------------------");
        System.out.printf("The Amount: %.0f\n", amount);
        System.out.printf("The KDV Rate: %.0f%%\n", kdvRatio);
@@ -37,7 +42,7 @@ public class App {
 
        System.out.printf("NET RESULT: %.2f\n", kdvResult);
        System.out.println("---------------------------");
-
+    
 
 
 
