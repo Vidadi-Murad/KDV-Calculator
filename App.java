22cc8da028948a69b293f473747b227822e2e5c0
VidadiMurad <vidad0murad@gmail.com>
2026-04-18 13:12:31 +0400

    Add Variables

--- App.java
+++ App.java
@@ -6,5 +6,18 @@ import java.util.Locale;
 
 
 public class App {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        
+        // Variables
+        int kdvAmount;
+        int kdvRatio;
+
+
+
+
+
+        scanner.close();
+    }
 }
